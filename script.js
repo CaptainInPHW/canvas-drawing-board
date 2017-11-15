a53ed1d8f56cb743724c2d4114f628e125b47537
Captain <1319836729@qq.com>
2017-11-15 23:45:12 +0800

    铅笔鼠标

--- script.js
+++ script.js
@@ -1,54 +1,92 @@
+
+var body = document.querySelector("body");
 var canvas = document.querySelector("canvas");
 var ctx = canvas.getContext('2d');
-// console.log(canvas);
 var last_position = {
 	offsetX: undefined,
 	offsetY: undefined
 }
 var draw_begin = false;
+var canvas_leave = false;
+canvas.onmouseenter = function(){
+	if (canvas_leave) {
+		draw_begin = false;
+	}
+}
+canvas.onmouseleave = function(){
+	canvas_leave = true;
+}
+body.onmouseup = function(){
+	if (canvas_leave) {
+		draw_begin = false;
+	}
+}
+body.onmouseleave = function(){
+	draw_begin = false;
+}
 canvas.onmousedown = function(evt){
-	draw_begin = !draw_begin;
-	// console.log(evt);
-	last_position.offsetX = evt.offsetX + 3;
-	last_position.offsetY = evt.offsetY + 27 ;
-} 
+	if (evt.which == 1) {
+		draw_begin = true;
+		last_position.offsetX = evt.offsetX;
+		last_position.offsetY = evt.offsetY;
+		// last_position.offsetX = evt.offsetX + 3;
+		// last_position.offsetY = evt.offsetY + 27 ;
+	}
+}
 canvas.onmousemove = function(evt){
 	if (draw_begin) {
-		if (last_position.offsetY < 30) {
-
-		}
 		var new_position = {
-			offsetX: evt.offsetX + 3,
-			offsetY: evt.offsetY + 27
-		}
+			offsetX: evt.offsetX,
+			offsetY: evt.offsetY
+			// offsetX: evt.offsetX + 3,
+			// offsetY: evt.offsetY + 27
+		};
 		ctx.beginPath();
 		ctx.moveTo(last_position.offsetX,last_position.offsetY);
 	    ctx.lineTo(new_position.offsetX,new_position.offsetY);
 	    ctx.closePath();
-		ctx.strokeStyle = "red";
+		// ctx.strokeStyle = "red";
 	    ctx.stroke();
 	    last_position = new_position;
 	}
 }
 canvas.onmouseup = function(evt){
-	draw_begin = !draw_begin;
+	draw_begin = false;
 }
 
 
 
 
 setCanvasSize();
-
 function setCanvasSize(){
 	var canvas = document.querySelector("canvas");
     canvas.width = document.documentElement.clientWidth;
     canvas.height = document.documentElement.clientHeight;
-  }
+}
+window.onresize = function(){
+	var canvas = document.querySelector("canvas");
+	var data_url = canvas.toDataURL();
+	setCanvasSize();
+	var image = new Image();
+	image.onload = function(){
+		ctx.drawImage(image, 0 ,0);
+	}
+ 	image.src = data_url;
+}
+
+
+usingPencil();
+function usingPencil(){
+	var canvas = document.querySelector("canvas");
+	var pencil = document.querySelector(".pencil");
+	pencil.onclick = function(){
+		canvas.className = "pencil";
+	};
+}
 usingPen();
 function usingPen(){
 	var canvas = document.querySelector("canvas");
 	var pen = document.querySelector(".pen");
-	console.log(pen);
 	pen.onclick = function(){
 		canvas.className = "pen";
 	};
@@ -63,6 +101,10 @@ function usingEraser(){
 }
 usingColor();
 function usingColor(){
+}
+
+
+function drowConfig(){
 	var color = document.querySelector(".color");
-	console.log(color.value);
+	// var 
 }
